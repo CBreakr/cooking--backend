48f938457fa0d4f828618f0004e37b746cbd0fba
CBreakr <github email address>
2020-06-23 13:08:37 -0400

    removed trailing / from production.rb host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -103,6 +103,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: "https://cookbook-social-server.herokuapp.com/" } 
-  Rails.application.routes.default_url_options[:host] = "https://cookbook-social-server.herokuapp.com/"
+  config.action_mailer.default_url_options = { host: "https://cookbook-social-server.herokuapp.com" } 
+  Rails.application.routes.default_url_options[:host] = "https://cookbook-social-server.herokuapp.com"
 end
