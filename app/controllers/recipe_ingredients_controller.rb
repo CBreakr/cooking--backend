8ec1d2b9ff1a45f011567b541e1f180ba4c981e5
CBreakr <github email address>
2020-06-23 13:16:34 -0400

    add back the recipe ingredients controller for heroku error

--- app/controllers/recipe_ingredients_controller.rb
+++ app/controllers/recipe_ingredients_controller.rb
@@ -1,4 +1,4 @@
-# class RecipeIngredientsController < ApplicationController
-#     ### I'm not sure we need the routes here
-#     ### since I think this will be handled in the recipe interaction
-# end
+class RecipeIngredientsController < ApplicationController
+    ### I'm not sure we need the routes here
+    ### since I think this will be handled in the recipe interaction
+end
