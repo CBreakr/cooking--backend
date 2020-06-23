728b98df7f923056a679bad53a98e66f72424d11
CBreakr <github email address>
2020-06-23 13:54:14 -0400

    recipe tags controller

--- app/controllers/recipe_tags_controller.rb
+++ app/controllers/recipe_tags_controller.rb
@@ -1,4 +1,4 @@
-# class RecipeTagsController < ApplicationController
-#     ### I'm not sure we need the routes here
-#     ### since I think this will be handled in the recipe interaction
-# end
+class RecipeTagsController < ApplicationController
+    ### I'm not sure we need the routes here
+    ### since I think this will be handled in the recipe interaction
+end
